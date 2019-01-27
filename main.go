4986a2ec8de226482792dcbbc07b8e487f1cab47
tonychg <tonychg>
2019-01-27 01:35:53 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+
+import (
+	"github.com/tonychg/libgen-cli/cmd"
+	// "fmt"
+)
+
+
+func main() {
+	// const (
+	// 	search = "Kubernetes: Up and Running"
+	// )
+	cmd.Execute()
+	// fmt.Println(args)
+	// libgen.RequestBooks(search)
+}
