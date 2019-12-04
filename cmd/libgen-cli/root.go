96b78045f8914267441e174dfbee0ec71ea659eb
Ryan <ryan@ciehanski.com>
2019-12-04 00:33:33 -0600

    fixed broken mirrors + major refactor

---
+++ cmd/libgen-cli/root.go
@@ -0,0 +1,37 @@
+// Copyright © 2019 Antoine Chiny <antoine.chiny@inria.fr>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package libgen_cli
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "libgen-cli",
+	Short: "CLI wrapper of the libgen.io API",
+	Long: `
+This CLI, List all downloads urls for a specify pattern
+It searches all the md5 hash on libgen.io. Get the file from booksdl.org`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() error {
+	if err := rootCmd.Execute(); err != nil {
+		return err
+	}
+	return nil
+}
